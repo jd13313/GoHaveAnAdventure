01551abd2c57610b9157194ff910161ede062f95
Jeremy Duenas <jeremy.duenas@koddi.com>
2024-04-13 19:26:15 -0400

    idk

---
+++ utils/Color.go
@@ -0,0 +1,21 @@
+package utils
+
+import "strconv"
+
+type Color struct {
+	r int
+	g int
+	b int
+}
+
+func CreateColor(r int, g int, b int) Color {
+	return Color{r, g, b}
+}
+
+func (c Color) GetANSI() string {
+	rStr := strconv.Itoa(c.r)
+	gStr := strconv.Itoa(c.g)
+	bStr := strconv.Itoa(c.b)
+
+	return "\033[38;2;" + rStr + ";" + gStr + ";" + bStr + "m"
+}
